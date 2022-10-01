8badc6b5bfbd8f9536593f661965a22f78c0da52
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2022-10-02 00:29:07 +0200

    builder pattern

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+
+public class LoopTest {
+
+    @Test
+    void testLoopAndConditionalBreakpoints() {
+
+    }
+
+    @Test
+    void testLoop() {
+
+    }
+}
